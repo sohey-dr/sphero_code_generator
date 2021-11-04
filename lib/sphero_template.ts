939456baccb733ee5edd7288dc6eb3adb9b00eb5
soheyHashimoto <heyso1209@gmail.com>
2021-11-04 13:04:09 +0900

    [ADD]テンプレートを用意

--- lib/sphero_template.ts
+++ lib/sphero_template.ts
@@ -1,3 +1,13 @@
-function main() {
-  console.log("Hello World!");
-}
+const { Scanner, Utils } = require("spherov2.js");
+
+const run = async () => {
+  const sphero = await Scanner.findSpheroMini();
+
+  if (!sphero) return console.log("sphero mini not available!");
+
+  // Write the code here!
+
+
+};
+
+run();
