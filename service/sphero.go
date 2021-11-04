f0dfdb349de108b7a7a6c779c44c805bb55d6f61
soheyHashimoto <heyso1209@gmail.com>
2021-11-04 14:36:25 +0900

    [ADD]toyをspheroに置き換える

--- service/sphero.go
+++ service/sphero.go
@@ -3,6 +3,6 @@ package service
 type Element string
 
 const (
-    GO_FORWARD  Element = "await toy.rollTime(SPEED, 0, PATROL_TIME, []);"
-    GO_BACKWARD Element = "await toy.rollTime(SPEED, 180, PATROL_TIME, []);"
+    GO_FORWARD  Element = "await sphero.rollTime(SPEED, 0, PATROL_TIME, []);"
+    GO_BACKWARD Element = "await sphero.rollTime(SPEED, 180, PATROL_TIME, []);"
 )
