fdaa259b436188fc0cc72b8ef8f26ca967a6ebea
soheyHashimoto <heyso1209@gmail.com>
2021-11-04 19:00:04 +0900

    [MODIFY]コメント修正

--- service/sphero.go
+++ service/sphero.go
@@ -3,7 +3,7 @@ package service
 type Element string
 
 const (
-    # TODO: スピードと時間を変えられるようにしたい
+    // TODO: スピードと時間を変えられるようにしたい
     GO_FORWARD  Element = "await sphero.rollTime(250, 0, 2000, []);"
     GO_BACKWARD Element = "await sphero.rollTime(250, 180, 2000, []);"
     GO_RIGHT    Element = "await sphero.rollTime(250, 90, 2000, []);"
